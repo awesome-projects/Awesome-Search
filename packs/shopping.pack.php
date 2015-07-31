b630e91cce4aeff3086306ff931ede309af59cd7
Pinetrax <jannekwinter@outlook.com>
2015-07-31 14:58:18 +0200

    Added amazon suggestions, added ebay

--- packs/shopping.pack.php
+++ packs/shopping.pack.php
@@ -3,7 +3,15 @@
 	$searches[] = array(
 		"name" => "Amazon",
 		"prefix" => "a",
-		"url" => "http://www.amazon.com/exec/obidos/external-search/?field-keywords={searchTerms}&mode=blended"
+		"url" => "http://www.amazon.com/exec/obidos/external-search/?field-keywords={searchTerms}&mode=blended",
+        "suggestions" => "http://completion.amazon.co.uk/search/complete?method=completion&q={searchTerms}&search-alias=aps&mkt=4"
 	);
+
+    $searches[] = array(
+        "name" => "Ebay",
+        "prefix" => "e",
+        "url" => "http://shop.ebay.de/?_nkw={searchQuery}",
+        "suggestions" => "http://anywhere.ebay.com/services/suggest/?s=0&q={searchQuery}"
+    );
 	
 ?>
