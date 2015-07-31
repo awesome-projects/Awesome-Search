d19c02c4ba2075bf8cd68db93c067b653dc64447
Tim Weißenfels <tim-we-github@outlook.co.nz>
2015-07-31 14:48:35 +0200

    introducing packs
    containing shopping pack

---
+++ packs/shopping.pack.php
@@ -0,0 +1,9 @@
+<?php
+	
+	$searches[] = array(
+		"name" => "Amazon",
+		"prefix" => "a",
+		"url" => "http://www.amazon.com/exec/obidos/external-search/?field-keywords={searchTerms}&mode=blended"
+	);
+	
+?>
