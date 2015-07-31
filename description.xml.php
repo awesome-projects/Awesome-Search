99b7b8860869f77e3b5b0c65af36d07bef64aa19
Tim Weißenfels <tim-we-github@outlook.co.nz>
2015-08-01 01:49:14 +0200

    static suggestions

--- description.xml.php
+++ description.xml.php
@@ -1,9 +1,12 @@
 <?php
-	//header('Content-type: text/xml; charset=utf-8'); //Firefox Addon Store
-	header('Content-type: application/opensearchdescription+xml;charset=UTF-8'); //Apple Discussions Forum
+	
+	//header('Content-type: text/xml; charset=utf-8');
+	header('Content-type: application/opensearchdescription+xml;charset=UTF-8');
+	
 ?><?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
 	<ShortName>Unified Search</ShortName>
 	<Description>Use unifysear.ch to search the Web.</Description>
 	<Url type="text/html" template="http://unifysear.ch/?q={searchTerms}"/>
+	<Url type="application/x-suggestions+json" template="http://unifysear.ch/suggestions.php?q={searchTerms}"/>
 </OpenSearchDescription>
--- suggestions.php
+++ suggestions.php
@@ -1,3 +1,26 @@
 <?php
 	
+	if(!isset($_GET["q"])) { die(42); }
+	
+	ini_set('display_errors',1);
+	error_reporting(E_ALL|E_STRICT);
+	ini_set('error_log','script_errors.log');
+	ini_set('log_errors','On');
+	
+	include_once("core.php");
+	
+	$query = preg_replace('/\s+/', ' ', trim($_GET["q"]) );
+	
+	if(strlen($query) > 512) { $query = substr($query, 0, 512); }
+	
+	
+	$suggestions = array(
+		$_GET["q"],
+		array("test 42", "test awesome", "test static"),
+		array("42","2x21","6x7"),
+		array()
+	);
+	
+	echo json_encode($suggestions);
+	
 ?>
