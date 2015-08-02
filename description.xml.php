91c090b5708f06befc15ecba884704fd8ef3c16c
Pinetrax <jannekwinter@outlook.com>
2015-08-03 00:42:48 +0200

    url in description for search now based on variable

--- description.xml.php
+++ description.xml.php
@@ -2,12 +2,13 @@
 	
 	//header('Content-type: text/xml; charset=utf-8');
 	header('Content-type: application/opensearchdescription+xml;charset=UTF-8');
+    $url = "http://unifysear.ch/";
 	
 	//$url = str_replace("&", "&amp;", $url);
 ?><?xml version="1.0" encoding="UTF-8"?>
 <OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
 	<ShortName>Unified Search</ShortName>
 	<Description>Use unifysear.ch to search the Web.</Description>
-	<Url type="text/html" template="http://unifysear.ch/search?q={searchTerms}"/>
-	<Url type="application/x-suggestions+json" template="http://unifysear.ch/suggestions.php?q={searchTerms}"/>
+	<Url type="text/html" template="<?php echo $url; ?>search?q={searchTerms}"/>
+	<Url type="application/x-suggestions+json" template="<?php echo $url; ?>suggestions.php?q={searchTerms}"/>
 </OpenSearchDescription>
